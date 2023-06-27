37b1b60c1c2e35148c5ac4a4d12c148f3d03025c
Janez <janezjovanovic04@gmail.com>
2023-06-27 23:34:37 +0200

    refactor: rename utils folder to file

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/utils/cn";
+import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, forwardRef } from "react";
 
