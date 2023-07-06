cbb9e1ed0c5f543f2aec98f0f5693e0b4e98f9f9
Jankela04 <janezjovanovic04@gmail.com>
2023-07-06 10:46:36 +0200

    feat: create section + fix hero header padding

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             src="/images/bg-mammoth-image.jpg"
           />
         </div>
-        <div className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform px-8 text-center text-white md:px-16">
+        <div className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform px-8 text-center text-white md:px-10">
           <h1
             className={cn(poppins.className, "text-5xl font-bold xl:text-7xl")}
           >
@@ -30,13 +30,40 @@ export default function Home() {
           <Button className="mt-8">Shop Now</Button>
         </div>
       </section>
-      <section className="bg-neutral-700 text-white">
+      <section className="flex flex-col gap-24 bg-neutral-700 text-white">
+        <div className="content-container pt-20 text-center">
+          <h2
+            className={cn(
+              poppins.className,
+              "pb-10 text-4xl font-bold text-primary-500 lg:text-5xl"
+            )}
+          >
+            Mammoths for your everyday life
+          </h2>
+          <p className="mx-auto max-w-3xl">
+            Discover the awe-inspiring world of mammoths and bring their
+            enchantment into your life. Our mammoth collection offers
+            extraordinary companionship and endless possibilities. From
+            delightful playtime to unforgettable adventures, our mammoths are
+            here to ignite your imagination and create lifelong memories.
+            Explore our selection today and experience the magic of owning a
+            mammoth firsthand.
+          </p>
+        </div>
         <div className="content-container flex flex-col items-center justify-center gap-4 text-center md:flex-row md:justify-between md:gap-10">
+          <div>
+            <Image
+              src="/images/mammoth-transparent.png"
+              alt="Mammoth"
+              width={540}
+              height={510}
+            />
+          </div>
           <div>
             <h2
               className={cn(
                 poppins.className,
-                "py-10 text-3xl font-bold text-primary-500"
+                "py-10 text-3xl font-bold text-primary-500 xl:text-4xl"
               )}
             >
               Discover Mammoth Connections
@@ -46,14 +73,6 @@ export default function Home() {
               pet, and bond with these pets. Discover the joy of real-time
               coexistence and forge lasting connections that enrich your life.
             </p>
-          </div>
-          <div>
-            <Image
-              src="/images/mammoth-transparent.png"
-              alt="Mammoth"
-              width={540}
-              height={510}
-            />
           </div>
         </div>
       </section>
