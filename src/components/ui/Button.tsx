35ca6fa9b5842b704734a851cdf8124412b56e7d
Jankela04 <janezjovanovic04@gmail.com>
2023-07-13 22:03:23 +0200

    fix: button type variant + add outline variant

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,15 +4,17 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, forwardRef } from "react";
 
 const buttonVariants = cva(
-  "inline-flex align-center justify-center py-2 px-4 rounded-lg text-lg",
+  "inline-flex align-center justify-center py-2 px-4 rounded-lg text-lg focus:ring-2 ring-blue-500",
   {
     variants: {
-      type: {
+      variant: {
         primary: "bg-primary-500 font-bold hover:bg-primary-600",
+        outline:
+          "border border-white bg-background hover:bg-gray-800/50 border-input",
       },
     },
     defaultVariants: {
-      type: "primary",
+      variant: "primary",
     },
   }
 );
@@ -21,11 +23,11 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, variant, ...props }, ref) => {
     return (
       <button
         {...props}
-        className={cn(buttonVariants({ type, className }))}
+        className={cn(buttonVariants({ variant, className }))}
         ref={ref}
       />
     );
